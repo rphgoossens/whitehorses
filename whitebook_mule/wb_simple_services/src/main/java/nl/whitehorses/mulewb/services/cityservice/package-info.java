a66adf07164ca248be480fc3818ccfc3c86c88e3
rphgoossens <r.p.h.goossens@gmail.com>
2016-02-27 12:56:42 +0100

    Added whitebook mule and simple services

---
+++ whitebook_mule/wb_simple_services/src/main/java/nl/whitehorses/mulewb/services/cityservice/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://services.mulewb.whitehorses.nl/CityService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package nl.whitehorses.mulewb.services.cityservice;
---
+++ whitebook_mule/wb_simple_services/src/main/java/nl/whitehorses/mulewb/services/rest/CityService.java
@@ -0,0 +1,9 @@
+package nl.whitehorses.mulewb.services.rest;
+
+import java.util.List;
+
+import nl.whitehorses.mulewb.model.City;
+
+public interface CityService {
+	public abstract List<City> getCities();
+}
