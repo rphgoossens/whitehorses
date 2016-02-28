868b8f3ad900e613fc4c312e4ad49d9dc7be80d5
r.p.h.goossens@gmail.com <r.p.h.goossens@gmail.com>
2016-02-28 19:44:28 +0100

    Deleted project wb_simple_services

--- whitebook_mule/wb_simple_services/src/main/java/nl/whitehorses/mulewb/services/cityservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://services.mulewb.whitehorses.nl/CityService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package nl.whitehorses.mulewb.services.cityservice;
--- whitebook_mule/wb_simple_services/src/main/java/nl/whitehorses/mulewb/services/rest/CityService.java
+++
@@ -1,9 +0,0 @@
-package nl.whitehorses.mulewb.services.rest;
-
-import java.util.List;
-
-import nl.whitehorses.mulewb.model.City;
-
-public interface CityService {
-	public abstract List<City> getCities();
-}
